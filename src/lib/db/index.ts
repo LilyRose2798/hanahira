4b8f50d62a4bb74c1fa5e18b4d56b6e99681dc60
LilyRose2798 <lily@rose.place>
2023-11-13 03:02:47 +1100

    Add db connection timeouts to fix connections not being closed

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -4,7 +4,8 @@ import { env } from "@/lib/env.mjs"
 import { schema } from "@/lib/db/schema"
 import { customAlphabet } from "nanoid"
 
-export const client = postgres(env.DATABASE_URL)
+// eslint-disable-next-line camelcase
+export const client = postgres(env.DATABASE_URL, { idle_timeout: 30, max_lifetime: 600 })
 export const db = drizzle(client, { schema })
 
 export const nanoid = customAlphabet(env.NANOID_ALPHABET, env.NANOID_SIZE)
