e5edb5f10c18cfd70f1f9add6a66ea4d1f948677
LilyRose2798 <lily@rose.place>
2023-11-17 21:52:46 +1100

    Fix config for db query

--- src/lib/db/utils.ts
+++ src/lib/db/utils.ts
@@ -3,16 +3,19 @@ import { PgColumn } from "drizzle-orm/pg-core"
 
 export const sqlDefault = sql`DEFAULT`
 export type SQLDefaults<T> = { [_ in { [K in keyof T]-?: undefined extends T[K] ? K : never }[keyof T]]: SQL }
+
 export const paginationConfig = ({ page = 1, pageSize = 50 }: { page?: number, pageSize?: number }) => ({
   offset: (page - 1) * pageSize,
   limit: pageSize,
 })
-export const sortingConfig = <T extends Record<string, unknown>>(sort?: string) => (table: Record<keyof T, PgColumn<any>>) => ({
-  orderBy: sort?.split(",").map(x => table[x]),
+
+export const sortingConfig = <T extends Record<string, unknown>>(sort?: string) => ({
+  orderBy: sort ? (table: Record<keyof T, PgColumn<any>>) => sort.split(",").map(x => table[x]) : undefined,
 })
-export const whereConfig = <T extends Record<string, unknown>>(query: Partial<T>, keys: (keyof T)[] = Object.keys(query)) => (
-  table: Record<keyof T, PgColumn<any>>,
-  { and, eq }: { and: (...conditions: (SQLWrapper | undefined)[]) => SQL<unknown> | undefined, eq: BinaryOperator }
-) => ({
-  where: and(...keys.map(k => (pk => (pk === undefined ? undefined : eq(table[k], pk)))(query[k]))),
+
+export const whereConfig = <T extends Record<string, unknown>>(query: Partial<T>, keys: (keyof T)[] = Object.keys(query)) => ({
+  where: (
+    table: Record<keyof T, PgColumn<any>>,
+    { and, eq }: { and: (...conditions: (SQLWrapper | undefined)[]) => SQL<unknown> | undefined, eq: BinaryOperator }
+  ) => and(...keys.map(k => (pk => (pk === undefined ? undefined : eq(table[k], pk)))(query[k]))),
 })
