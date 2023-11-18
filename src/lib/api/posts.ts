a35a43effed0c70aa6466c199d7c7663286fac56
LilyRose2798 <lily@rose.place>
2023-11-18 19:03:36 +1100

    Update user and post api

--- src/lib/api/posts.ts
+++ src/lib/api/posts.ts
@@ -1,7 +1,7 @@
 import { db } from "@/lib/db"
 import { whereConfig, paginationConfig, sortingConfig } from "@/lib/db/utils"
 import { eq } from "drizzle-orm"
-import { postDefaults, PostIdParams, PostCreatedByParams, CreatePostParams, ReplacePostParams, UpdatePostParams, QueryPostParams } from "@/lib/db/schemas/posts"
+import { postDefaults, PostIdParams, PostCreatedByParams, PostModifiedByParams, CreatePostParams, ReplacePostParams, UpdatePostParams, QueryPostParams } from "@/lib/db/schemas/posts"
 import { posts } from "@/lib/db/tables/posts"
 import { parseFound, parseCreated, parseFoundFirst } from "@/lib/api/utils"
 import nanoid from "@/lib/db/nanoid"
@@ -17,10 +17,10 @@ export const findPostsCreatedByUsername = ({ username }: UsernameParams) => db.q
   .findFirst({ with: { posts: true }, where: (users, { eq }) => eq(users.username, username) })
   .execute().then(parseFound).then(x => x.posts)
 export const createPost = (post: CreatePostParams & PostCreatedByParams) => db.insert(posts)
-  .values({ ...post, id: nanoid(), modifiedBy: post.createdBy, modifiedAt: new Date() }).returning().execute().then(parseCreated)
-export const replacePost = ({ id, ...post }: ReplacePostParams & PostCreatedByParams) => db.update(posts)
-  .set({ ...postDefaults, ...post }).where(eq(posts.id, id)).returning().execute().then(parseFoundFirst)
-export const updatePost = ({ id, ...post }: UpdatePostParams & PostCreatedByParams) => db.update(posts)
-  .set(post).where(eq(posts.id, id)).returning().execute().then(parseFoundFirst)
+  .values({ ...post, id: nanoid(), modifiedBy: post.createdBy }).returning().execute().then(parseCreated)
+export const replacePost = ({ id, ...post }: ReplacePostParams & PostModifiedByParams) => db.update(posts)
+  .set({ ...postDefaults, ...post, modifiedAt: new Date() }).where(eq(posts.id, id)).returning().execute().then(parseFoundFirst)
+export const updatePost = ({ id, ...post }: UpdatePostParams & PostModifiedByParams) => db.update(posts)
+  .set({ ...post, modifiedAt: new Date() }).where(eq(posts.id, id)).returning().execute().then(parseFoundFirst)
 export const deletePost = ({ id }: PostIdParams) => db.delete(posts)
   .where(eq(posts.id, id)).returning().execute().then(parseFoundFirst)
