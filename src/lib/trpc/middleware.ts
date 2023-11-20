679e4a320de61a84d2b78d9f2e79e35f8cb5de99
LilyRose2798 <lily@rose.place>
2023-11-20 11:11:10 +1100

    Update middleware

--- src/lib/trpc/middleware.ts
+++ src/lib/trpc/middleware.ts
@@ -6,29 +6,29 @@ import { UserRole, hasUserRole } from "@/lib/db/enums/userRole"
 import { validateAuth } from "@/lib/lucia"
 
 export const hasAuth = m()
-  .create(async ({ next, ...opts }) => {
-    const { session, user } = await validateAuth(opts.type === "mutation")
+  .create(async ({ type, next }) => {
+    const { session, user } = await validateAuth(type === "mutation")
     if (!session) throw new TRPCError({ code: "UNAUTHORIZED", message: "Not signed in" })
-    return next({ ...opts, ctx: { ...opts.ctx, session, user } })
+    return next({ ctx: { session, user } })
   })
 
 export const hasRole = (userRole: UserRole) => m<{ ctx: { user: User } }>()
-  .create(async ({ ctx, next }) => {
-    if (hasUserRole(ctx.user.role, userRole)) throw new TRPCError({ code: "FORBIDDEN", message: "Signed in user does not have the required role" })
+  .create(async ({ ctx: { user }, next }) => {
+    if (hasUserRole(user.role, userRole)) throw new TRPCError({ code: "FORBIDDEN", message: "Signed in user does not have the required role" })
     return next()
   })
 
 export const canEditUser = m<{ ctx: { user: User }, input: UserIdParams }>()
-  .create(async ({ ctx, input: { id }, next }) => {
-    if (hasUserRole(ctx.user.role, "SITE_MODERATOR")) return next()
-    if (id !== ctx.user.id) throw new TRPCError({ code: "FORBIDDEN", message: "Signed in user does not own user with specified ID and does not have required access level to override operation" })
+  .create(async ({ ctx: { user }, input: { id }, next }) => {
+    if (hasUserRole(user.role, "SITE_MODERATOR")) return next()
+    if (id !== user.id) throw new TRPCError({ code: "FORBIDDEN", message: "Signed in user does not own user with specified ID and does not have required access level to override operation" })
     return next()
   })
 
 export const canEditPost = m<{ ctx: { user: User }, input: PostIdParams }>()
-  .create(async ({ ctx, next, ...opts }) => {
-    const post = await findPostById({ id: opts.input.id })
-    if (hasUserRole(ctx.user.role, "POST_EDITOR")) return next({ ...opts, ctx: { ...ctx, post } })
-    if (post.createdBy !== ctx.user.id) throw new TRPCError({ code: "FORBIDDEN", message: "Signed in user does not own post with specified ID and does not have required access level to override operation" })
-    return next({ ...opts, ctx: { ...ctx, post } })
+  .create(async ({ ctx: { user }, input: { id }, next }) => {
+    const post = await findPostById({ id })
+    if (hasUserRole(user.role, "POST_EDITOR")) return next({ ctx: { post } })
+    if (post.createdBy !== user.id) throw new TRPCError({ code: "FORBIDDEN", message: "Signed in user does not own post with specified ID and does not have required access level to override operation" })
+    return next({ ctx: { post } })
   })
