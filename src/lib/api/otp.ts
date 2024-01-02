277b190084682a90aa865da5e69b8a9409e9f376
LilyRose2798 <lily@rose.place>
2024-01-02 20:49:42 +1100

    Enhance OTP functionality

--- src/lib/api/otp.ts
+++ src/lib/api/otp.ts
@@ -1,7 +1,10 @@
+import { toDataURL } from "qrcode"
+import { raise, someAsync } from "@/lib/utils"
+
 export const BASE32_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
 const BASE32_ALPHABET_IND = Object.fromEntries([...BASE32_ALPHABET].map((x, i) => [x, i]))
 
-export const base32Encode = (data: ArrayBufferLike, padding: boolean = false) => {
+export const base32Encode = (data: ArrayBuffer, padding: boolean = false) => {
   let bits = 0
   let value = 0
   let output = ""
@@ -30,14 +33,25 @@ export const base32Decode = (data: string) => {
   return output.buffer
 }
 
-// export const generateOTPKey = async (length: number = 10) => (await crypto.subtle.exportKey("raw", await crypto.subtle.generateKey({ name: "HMAC", hash: "SHA-1" }, true, ["sign"]))).slice(0, length)
-export const generateOTPKey = async (length: number = 10) => crypto.getRandomValues(new Uint8Array(length)).buffer
+export type OTPKey = string | ArrayBuffer | CryptoKey
+export type OTPAlgorithm = "SHA-1" | "SHA-256" | "SHA-512"
+export type OTPOpts = { digits?: number, algorithm?: OTPAlgorithm }
 
-export type OTPOpts = { digits?: 6 | 8, algorithm?: "SHA-1" | "SHA-256" | "SHA-512" }
+export const DEFAULT_OTP_KEY_BYTES = 20
+export const DEFAULT_OTP_DIGITS = 6
+export const DEFAULT_OTP_ALGORITHM = "SHA-1" satisfies OTPAlgorithm
+export const DEFAULT_OTP_PERIOD = 30
 
-export type HOTPOpts = OTPOpts & { counter: number }
+// export const generateOTPKey = async (bytes: number = DEFAULT_OTP_KEY_BYTES) => (await crypto.subtle.exportKey("raw", await crypto.subtle.generateKey({ name: "HMAC", hash: "SHA-1" }, true, ["sign"]))).slice(0, bytes)
+export const generateOTPKey = async (bytes: number = DEFAULT_OTP_KEY_BYTES) => crypto.getRandomValues(new Uint8Array(bytes)).buffer
 
-export const generateHOTP = async (key: string | ArrayBuffer | CryptoKey, { counter, digits = 6, algorithm = "SHA-1" }: HOTPOpts) => {
+type CounterOpts = { counter: number }
+export type HOTPOpts = OTPOpts & CounterOpts
+type WindowOpts = { window?: number }
+type OffsetOpts = { offset?: number }
+
+export const generateHOTP = async (key: OTPKey, { counter, digits = DEFAULT_OTP_DIGITS, algorithm = DEFAULT_OTP_ALGORITHM }: HOTPOpts) => {
+  if (digits < 1) raise("OTP digits must be greater than 0")
   const counterArr = new ArrayBuffer(8)
   new DataView(counterArr).setBigUint64(0, BigInt(counter))
   const cryptoKey = key instanceof CryptoKey ? key :
@@ -50,26 +64,69 @@ export const generateHOTP = async (key: string | ArrayBuffer | CryptoKey, { coun
   return trunc.toString().padStart(digits, "0")
 }
 
-export const verifyHOTP = async (hotp: string, ...args: Parameters<typeof generateHOTP>) => hotp === await generateHOTP(...args)
+const generateWindowOffsets = (window: number = 0) => (
+  window < 0 ? raise("OTP window must be 0 or more") :
+    [0, ...[...Array(Math.abs(window))].map((_, i) => i + 1).flatMap(x => [x, -x])])
 
-export type TOTPOpts = OTPOpts & { period?: number }
+export const verifyHOTP = async (hotp: string, key: OTPKey, { window, ...opts }: HOTPOpts & WindowOpts) => (
+  someAsync(generateWindowOffsets(window), async offset => hotp === await generateHOTP(key, { ...opts, counter: opts.counter + offset })))
 
-export const generateTOTP = async (key: string | ArrayBuffer | CryptoKey, { period = 30, ...opts }: TOTPOpts = {}) => (
-  generateHOTP(key, { counter: Math.floor(Date.now() / (period * 1000)), ...opts }))
+type PeriodOpts = { period?: number }
+export type TOTPOpts = OTPOpts & PeriodOpts
+type DateOpts = { date?: Date }
 
-export const verifyTOTP = async (totp: string, ...args: Parameters<typeof generateTOTP>) => totp === await generateTOTP(...args)
+export const generateTOTPCounter = ({ period = DEFAULT_OTP_PERIOD, date = new Date() }: PeriodOpts & DateOpts) => (
+  Math.floor(date.getTime() / (period * 1000)))
 
-export type OTPURIOpts = (HOTPOpts | TOTPOpts) & { name?: string, issuer?: string }
+export const generateTOTP = async (key: OTPKey, { period, date, offset = 0, ...opts }: TOTPOpts & DateOpts & OffsetOpts = {}) => (
+  generateHOTP(key, { counter: generateTOTPCounter({ period, date }) + offset, ...opts }))
 
-export const generateOTPURI = async (key: string | ArrayBuffer | CryptoKey, { digits, algorithm, name = "", issuer, ...opts }: OTPURIOpts = {}) => {
-  const uri = new URL(`otpauth://totp/${name}`)
-  uri.searchParams.set("secret", typeof key === "string" ? key :
+export const verifyTOTP = async (totp: string, key: OTPKey, { window, ...opts }: TOTPOpts & WindowOpts = {}) => (
+  someAsync(generateWindowOffsets(window), async offset => totp === await generateTOTP(key, { ...opts, offset })))
+
+export type OTPURIOpts = (HOTPOpts | TOTPOpts) & { issuer?: string } & ({} | { name: string } | { label: string, user: string })
+
+export const generateOTPURL = async (key: OTPKey, { digits, algorithm, issuer, ...opts }: OTPURIOpts = {}) => {
+  const isHOTP = "counter" in opts
+  const type = isHOTP ? "hotp" : "totp"
+  const name = "name" in opts ? opts.name :
+    "label" in opts ? `${opts.label ?? ""}:${opts.user ?? ""}` : ""
+  const url = new URL(`otpauth://${type}/${name}`)
+  url.searchParams.set("secret", typeof key === "string" ? key :
     base32Encode(key instanceof CryptoKey ?
       await crypto.subtle.exportKey("raw", key) : key))
-  if (issuer) uri.searchParams.set("issuer", issuer)
-  if (digits) uri.searchParams.set("digits", digits.toString())
-  if (algorithm) uri.searchParams.set("algorithm", algorithm.replace("-", ""))
-  if ("counter" in opts) uri.searchParams.set("counter", opts.counter.toString())
-  if ("period" in opts && opts.period) uri.searchParams.set("period", opts.period.toString())
-  return uri.toString()
+  if (issuer !== undefined) url.searchParams.set("issuer", issuer)
+  if (digits !== undefined) url.searchParams.set("digits", digits.toString())
+  if (algorithm !== undefined) url.searchParams.set("algorithm", algorithm.replace("-", ""))
+  if (isHOTP) url.searchParams.set("counter", opts.counter.toString())
+  else if (opts.period !== undefined) url.searchParams.set("period", opts.period.toString())
+  return url.toString()
 }
+
+export const parseOTPURL = async (url: string | URL) => {
+  const x = new URL(url)
+  if (x.protocol !== "otpauth:") raise("Invalid OTP URL protocol")
+  const name = decodeURIComponent(x.pathname.slice(1))
+  const param = <T extends string, U = string>(name: T, parse?: (x: string) => U) => {
+    const param = x.searchParams.get(name)
+    return (param === null ? {} : { [name]: parse ? parse(param) : param }) as { [K in T]?: U }
+  }
+  return {
+    ...(x.hostname === "hotp" ? { type: "hotp" as const, ...param("counter", x => +x) } :
+      x.hostname === "totp" ? { type: "totp" as const, ...param("period", x => +x) } :
+        raise("Invalid OTP URL type")),
+    ...(name.includes(":") ?
+      (([_, label, user]) => ({ label, user }))(name.match(/^(.*?):(.*?)$/) ?? raise("Invalid OTP URL name")) :
+      { name }),
+    key: x.searchParams.get("secret") ?? raise("Invalid OTP URL key"),
+    ...param("issuer"),
+    ...param("digits", x => +x),
+    ...param("algorithm", (x): OTPAlgorithm => (
+      x === "SHA1" ? "SHA-1" :
+        x === "SH256" ? "SHA-256" :
+          x === "SHA512" ? "SHA-512" :
+            raise("Invalid OTP URL algorithm"))),
+  }
+}
+
+export const generateOTPDataURL = (...args: Parameters<typeof generateOTPURL>) => generateOTPURL(...args).then(url => toDataURL(url))
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,9 +2,13 @@ import { clsx, ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
+
 export const nullToUndef = <T>(x: T): T & {} | undefined => (x !== null ? x : undefined)
+
 export const titleCase = (name: string) => name.split(" ").map(x => `${x[0].toUpperCase()}${x.substring(1)}`).join(" ")
+
 export const truncate = (val: string, len: number) => (val.length <= len ? val : `${val.slice(0, len - 3)}...`)
+
 export const humanFileSize = (bytes: number, si = false, dp = 1) => {
   const thresh = si ? 1000 : 1024
   if (Math.abs(bytes) < thresh) `${bytes} B`
@@ -18,6 +22,20 @@ export const humanFileSize = (bytes: number, si = false, dp = 1) => {
     u += 1
   } while (Math.round(Math.abs(b) * r) / r >= thresh && u < units.length - 1)
   return `${b.toFixed(dp)} ${units[u]}`
+}
+
+export const raise = (err: Error | string) => {
+  throw err instanceof Error ? err : new Error(err)
+}
+
+export const someAsync = async <T>(arr: T[], predicate: (val: T) => Promise<boolean>) => {
+  for (const e of arr) if (await predicate(e)) return true
+  return false
+}
+
+export const everyAsync = async <T>(arr: T[], predicate: (val: T) => Promise<boolean>) => {
+  for (const e of arr) if (!await predicate(e)) return false
+  return true
 }
 
 export type EnhancedOmit<TRecordOrUnion, KeyUnion> = string extends keyof TRecordOrUnion
